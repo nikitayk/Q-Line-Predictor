4be0e125862f78d3efa20bef1a126bda2d3ed68d
neetu54 <neetukundu54@gmail.com>
2025-07-07 23:34:01 +0530

    Integrated frontend with live backend

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ const QLineWaitTimePredictor = () => {
 
       console.log('Sending API request:', apiData);
 
-      const response = await fetch('http://localhost:5000/predict', {
+      const response = await fetch('https://q-line-backend.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
